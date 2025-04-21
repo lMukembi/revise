331c47234c949070c1864357690cafa64dbdb8f1
lMukembi <lmukembi@gmail.com>
2025-04-22 01:28:22 +0300

    files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const bodyParser = require("body-parser");
 const connectionDB = require("./connection.js");
 
 require("dotenv").config();
@@ -9,38 +8,45 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "https://revise.co.ke",
-      "https://www.revise.co.ke",
-      "https://api.revise.co.ke",
-      // "http://localhost:3000",
-    ],
-    credentials: true,
-    exposedHeaders: ["ip"],
-  })
-);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "client", "build")));
+
+const corsOptions = {
+  origin: [
+    "https://revise.co.ke",
+    "https://www.revise.co.ke",
+    "https://api.revise.co.ke",
+    // "http://localhost:3000",
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type, Authorization"],
+  credentials: true,
+  exposedHeaders: ["Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.use("/api/user", require("./routes/user"));
 app.use("/api/exams", require("./routes/exams"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/public/index.html"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 const DBConnection = async () => {
   try {
     await connectionDB(process.env.MONGO_URI);
 
-    app.listen(port, () => {
+    app.listen(port, "0.0.0.0", () => {
       console.log(`server is running on port, ${port}`);
     });
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     process.exit(1);
   }
 };
