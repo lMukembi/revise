668c02ab0947fa034807418e22e70a39032504db
lMukembi <lmukembi@gmail.com>
2025-05-03 00:14:32 +0300

    files

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const corsOptions = {
     "https://api.revise.co.ke",
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type, Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   exposedHeaders: ["Authorization"],
 };
