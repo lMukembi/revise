800dea32b2fbfbecada36aa105957674db9ea3a9
lMukembi <lmukembi@gmail.com>
2026-01-16 11:38:50 +0300

    files

--- server/routes/exams.js
+++ server/routes/exams.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
 
+const uploadDir = path.join(__dirname, "..", "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
+
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + file.originalname;
     cb(null, uniqueSuffix);
