c1e6686724931cafea28a077044cf4ae1ea59dfb
lMukembi <lmukembi@gmail.com>
2026-04-18 14:17:26 +0300

    files

--- client/src/components/examcard.js
+++ client/src/components/examcard.js
@@ -111,7 +111,7 @@ export const Examcard = ({ exam, index }) => {
           {index + 1}. {exam.code}: {exam.unit}
         </div>
         <div onClick={handleDownload} className="download" title="Download PDF">
-          <HiOutlineDocumentDownload className="downloadicon" />
+          <HiOutlineDocumentDownload className="actionbutton" />
         </div>
       </div>
       <div className="others">
